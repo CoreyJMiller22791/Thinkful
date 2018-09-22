4e83404067b5a5fbb551686f08aaf664d81f5b47
CoreyJMiller22791 <40246621+CoreyJMiller22791@users.noreply.github.com>
2018-09-22 13:24:10 -0400

    Delete max_temp.sql

--- max_temp.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	ZIP,
-	MAX(MaxTemperatureF) maximum_temp
-FROM 
-	weather,
-	stations
---ask about chaining queries to pull city name
--- may not be possible because no zip in stations where city data is
