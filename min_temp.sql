685fb93f447900192fbf0d84bae79bcb97d0bc27
CoreyJMiller22791 <40246621+CoreyJMiller22791@users.noreply.github.com>
2018-09-22 13:13:23 -0400

    Add files via upload

--- min_temp.sql
+++ min_temp.sql
@@ -1,7 +1,9 @@
 SELECT 
+	events,
 	MinTemperatureF
 FROM 
 	weather
 WHERE
 	ZIP = 94301 AND 
-	PrecipitationIn > 0
+	--PrecipitationIn > 0
+	(events = 'Rain' OR events = 'Fog-Rain' OR events = 'Rain-Thunderstorm')
